5dcef8c7c9b3b3844b55b3d57c300a744c6fe9ca
JP <joao.araujo@invillia.com>
2020-10-20 19:07:18 -0300

    Commit inicial

---
+++ consoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace consoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Console App!");
+
+            int n1 = 5;
+            int n2 = 2;
+
+            Console.WriteLine($"Resultado: {n1 + n2}");
+        }
+    }
+}
